6fcf4048ce269778b3231fb82367b79c6a4803b1
Tri <bayu165kusuma@gmail.com>
2020-06-24 17:05:30 +0700

    final array multidimensi

--- arraymultidimensi.js
+++ arraymultidimensi.js
@@ -1,9 +1,9 @@
-var count = 1;
+var count = 0;
 var array = []
 
-for(let i = 0; i < 4; i++) {
+for(let i = 0; i < 5; i++) {
     array[i] = []
-    for (let j =0; j < 5;  j++){
+    for (let j =0; j < 4;  j++){
         array[i][j] = count
         count++
     }
