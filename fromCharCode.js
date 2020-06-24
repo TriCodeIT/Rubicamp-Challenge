1f9fa09fc9e787d8c56b67bf3498c8f2d2407f13
TriCodeIT <67305098+TriCodeIT@users.noreply.github.com>
2020-06-24 20:52:49 +0700

    Delete fromCharCode.js

--- fromCharCode.js
+++
@@ -1,8 +0,0 @@
-/*
-fromCharCode ()
-Metode fromCharCode () mengubah nilai Unicode menjadi karakter
-*/
-
-var res = String.fromCharCode(65);// A
-
-var res = String.fromCharCode(72, 69, 76, 76, 79);// HELLO
